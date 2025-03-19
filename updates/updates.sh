bc6e1309f1711876c1b2053889ee33a30275cb9a
William Wood <wmwood1990@gmail.com>
2025-03-19 09:13:39 -0700

    Create updates.sh

---
+++ updates/updates.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt upgrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt clean -y
+sudo apt purge -y
+echo -e "\e[30;42m--------------- UPDATES COMPLETE ---------------\e[0m"
